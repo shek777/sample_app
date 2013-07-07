a3022e46604bf6dd58b5e3d09df511049ba188ac
Andrew Williams <aj.williams7@yahoo.com>
2013-07-07 13:28:34 -0500

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  # a4febccb798fc31f34aed9efbf5a75cf - aj.williams7@yahoo.com
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.strip.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  
 
   describe "signup page" do
     before { visit signup_path }
@@ -10,4 +11,13 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
+  
+  describe "profile page" do
+  # Replace with code to make a user variable
+  let(:user) { FactoryGirl.create(:user) }
+  before { visit user_path(user) }
+
+  it { should have_content(user.name) }
+  it { should have_title(user.name) }
+end
 end
